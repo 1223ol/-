bc2acd908d0aaa16024d9f04773b15a16c6dd63c
daiker <daikersec@gmail.com>
2018-04-24 12:21:22 +0800

    a

--- pages/result/index.js
+++ pages/result/index.js
@@ -4,10 +4,25 @@ var radarChart = null;
 Page({
   data: {
     array: ['雷达图', '柱状图', '扇形图', '折线图'],
-    index: 0
+    index: 0,
+    startDate: '2016-09-26',
+    endDate: '2016-09-26'
   },
   touchHandler: function (e) {
     console.log(radarChart.getCurrentDataIndex(e));
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var util = require('../../utils/util.js');
+    var time = util.formatDate(new Date());
+    // 再通过setData更改Page()里面的data，动态更新页面的数据  
+    this.setData({
+      startDate: time,
+      endDate: time
+    });
   },
   onReady: function (e) {
     var windowWidth = 320;
