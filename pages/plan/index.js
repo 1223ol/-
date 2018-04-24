43f0cce465335c7063d939090555c332048255fd
daiker <daikersec@gmail.com>
2018-04-24 11:23:46 +0800

    解决增加页面合并后的功能函数bug

--- pages/plan/index.js
+++ pages/plan/index.js
@@ -5,13 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    money:0.0,
+    money: 0.0,
+    billMoney: 0.0,
     startDate: '2016-09-26',
     endDate: '2016-09-26',
     date: '2016-09-26',
     array: ['饮食', '服饰装容', '生活日用', '住房缴费', '交通出行', "通讯", "文教娱乐", "健康", "其他消费"],
     index: 0,
-    selectItem:0
+    selectItem: 1
   },
 
   /**
@@ -23,7 +24,7 @@ Page({
     // 再通过setData更改Page()里面的data，动态更新页面的数据  
     this.setData({
       startDate: time,
-      endDate:time
+      endDate: time
     });
   },
 
@@ -33,19 +34,21 @@ Page({
   onReady: function () {
 
   },
-  changMoney:function(event){
-    this.setData({
-      money: event.detail.value
-    });
-  },
   addPlan:function(event){
+console.log("addPlan is invoked");
+  },
+  addBill: function (event) {
+    console.log("addBill is invoked");
+  },
+  addMyPlan: function (event) {
+    console.log("addPlan is invoke");
     var that = this;
     wx.request({
       url: 'http://127.0.0.1/addPlan', //仅为示例，并非真实的接口地址
       data: {
         money: this.data.money,
         startDate: this.data.startDate,
-        endDate:this.data.endDate
+        endDate: this.data.endDate
       },
       header: {
         'content-type': 'application/json' // 默认值
@@ -56,25 +59,41 @@ Page({
       }
     });
   },
+  changMoney: function (event) {
+    console.log("changMoney is invoke");
+    this.setData({
+      money: event.detail.value
+    });
+  },
+  changBillMoney: function (event) {
+    console.log("changBillMoney  is invoke");
+    this.setData({
+      billmoney: event.detail.value
+    });
+  },
   listenerDatePickerSelected: function (e) {
     //改变index值，通过setData()方法重绘界面
+    console.log("listenerDatePickerSelected  is invoke");
     this.setData({
-      date : e.detail.value
+      date: e.detail.value
     });
   },
   listenerPickerSelected: function (e) {
+    console.log("listenerPickerSelected  is invoke");
     //改变index值，通过setData()方法重绘界面
     this.setData({
       index: e.detail.value
     });
   },
   listenerStartDatePickerSelected: function (e) {
+    console.log("listenerStartDatePickerSelected  is invoke");
     //调用setData()重新绘制
     this.setData({
       startDate: e.detail.value,
     });
   },
   listenerEndDatePickerSelected: function (e) {
+    console.log("listenerEndDatePickerSelected  is invoke");
     //调用setData()重新绘制
     this.setData({
       endDate: e.detail.value,
@@ -126,18 +145,20 @@ Page({
 
 
 
-    jumpToAddBill: function (event) {
-      this.setData({
-        selectItem:0
-      });
+  jumpToAddBill: function (event) {
+    console.log("jumpToAddBill  is invoke");
+    this.setData({
+      selectItem: 0
+    });
 
-    },
+  },
 
-    jumpToPlan: function (event) {
-      this.setData({
-        selectItem: 1
-      });
+  jumpToPlan: function (event) {
+    console.log("jumpToPlan  is invoke");
+    this.setData({
+      selectItem: 1
+    });
 
-    },
+  },
 
 })
