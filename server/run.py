5ec71d70d95922396ea831a1e834a6cee9b94da2
daiker <daikersec@gmail.com>
2018-04-25 18:58:29 +0800

    添加https密钥证书

--- server/run.py
+++ server/run.py
@@ -7,4 +7,4 @@
 
 from flask import Flask
 from app import app
-app.run(debug = True,port=80)
+app.run(debug = True,host="0.0.0.0",port=80,ssl_context=('ssl/tally.slickghost.com.crt', 'ssl/tally.slickghost.com.key'))
