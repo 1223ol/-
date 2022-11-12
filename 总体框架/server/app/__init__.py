5f5c83b92c236d92a8ce6e5f083eede282a03430
呵734 <609592669@qq.com>
2022-11-12 10:27:18 +0800

    Signed-off-by: 呵734 <609592669@qq.com>

--- 总体框架/server/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,g
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-app.secret_key='daiker666'
-app.config.from_object('config')
-db = SQLAlchemy(app)
-from app import views,models
